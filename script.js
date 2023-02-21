747133d77bcba6c3739ed57580dd60fd1523c539
xbiotech <biotech9261@gmail.com>
2023-02-21 16:01:25 +0100

    ADD operate function

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    if(operator === "+"){
+        return add(num1, num2);
+    } else if (operator === "-"){
+        return substract(num1, num2);
+    } else if (operator === "*"){
+        return multiply(num1, num2);
+    } else if (operator === "/"){
+        return divide(num1, num2);
+    }
+}
